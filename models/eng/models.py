1536a297288e14c78687bdc4cfb94a0d87aa1d4d
shel-ho <business.sph@gmail.com>
2025-04-24 15:31:12 -0400

    Changed class structure

--- models/eng/models.py
+++ models/eng/models.py
@@ -4,9 +4,12 @@ from transformers import (AutoTokenizer, DataCollatorForTokenClassification, Aut
 import typing
 import evaluate
 import numpy as np
+from abc import ABC
 
-class NERModel:
-    def __init__(self, model_path: str) -> None:
+class NERModel(ABC):
+    def __init__(self, model_path: str, lr: float = 2e-5,
+                 train_batch_size: int = 16, num_epochs: int = 2,
+                 seed: int = 42, da: bool = False) -> None:
         self.model_path = model_path
         self.tokenizer = AutoTokenizer.from_pretrained(model_path)
         self.data_collator = DataCollatorForTokenClassification(tokenizer=self.tokenizer)
@@ -15,29 +18,30 @@ class NERModel:
         self.da_train_data = None
         self.lower_test = None
         self.upper_test = None
-        self.baseline = None
-        self.da = None
+        self.model = None
+        self.da = da
         self.training_args = TrainingArguments(
             output_dir="test_ner_model",
-            learning_rate=2e-5,
-            per_device_train_batch_size=16,
+            learning_rate=lr,
+            per_device_train_batch_size=train_batch_size,
             per_device_eval_batch_size=16,
-            num_train_epochs=2,
+            num_train_epochs=num_epochs,
             weight_decay=0.01,
             eval_strategy="epoch",
             save_strategy="epoch",
             load_best_model_at_end=True,
             push_to_hub=False,
             report_to='none',
+            seed=seed
         )
 
     def model_init(self):
         return AutoModelForTokenClassification.from_pretrained(
-        self.model_path, num_labels=9, id2label=id2label, label2id=label2id
-    )
+            self.model_path, num_labels=9, id2label=id2label, label2id=label2id
+        )
 
-    #Figure out how to type annotate dataset dicts, but this will do for now
-    def tokenize_and_align_labels(self, examples: dict[str,list[str]]) -> dict[str,list[str]]:
+    # Figure out how to type annotate dataset dicts, but this will do for now
+    def tokenize_and_align_labels(self, examples: dict[str, list[str]]) -> dict[str, list[str]]:
         tokenized_inputs = self.tokenizer(examples["tokens"], truncation=True, is_split_into_words=True)
 
         labels = []
@@ -64,8 +68,8 @@ class NERModel:
         self.lower_test = lower_test.map(self.tokenize_and_align_labels, batched=True)
         self.upper_test = upper_test.map(self.tokenize_and_align_labels, batched=True)
 
-    #I... actually do not know what p is, whoops
-    def compute_metrics(self, p) -> dict[str,float]:
+    # I... actually do not know what p is, whoops
+    def compute_metrics(self, p) -> dict[str, float]:
         predictions, labels = p
         predictions = np.argmax(predictions, axis=2)
 
@@ -86,41 +90,39 @@ class NERModel:
             "accuracy": results["overall_accuracy"],
         }
 
-    def baseline_train(self) -> None:
-        self.baseline = Trainer(
-            model_init=self.model_init,
-            args=self.training_args,
-            train_dataset=self.conll["train"],
-            eval_dataset=self.conll["test"],
-            processing_class=self.tokenizer,
-            data_collator=self.data_collator,
-            compute_metrics=self.compute_metrics,
-        )
+    def train(self) -> None:
+        if self.da:
+            self.model = Trainer(
+                model_init=self.model_init,
+                args=self.training_args,
+                train_dataset=self.da_train_data,
+                eval_dataset=self.conll["test"],
+                processing_class=self.tokenizer,
+                data_collator=self.data_collator,
+                compute_metrics=self.compute_metrics,
+            )
 
-        self.baseline.train()
+            self.model.train()
+        else:
+            self.model = Trainer(
+                model_init=self.model_init,
+                args=self.training_args,
+                train_dataset=self.conll["train"],
+                eval_dataset=self.conll["test"],
+                processing_class=self.tokenizer,
+                data_collator=self.data_collator,
+                compute_metrics=self.compute_metrics,
+            )
 
-    def da_train(self) -> None:
-        self.da = Trainer(
-            model_init=self.model_init,
-            args=self.training_args,
-            train_dataset=self.da_train_data,
-            eval_dataset=self.conll["test"],
-            processing_class=self.tokenizer,
-            data_collator=self.data_collator,
-            compute_metrics=self.compute_metrics,
-        )
-
-        self.da.train()
+            self.model.train()
 
     def eval(self) -> None:
-        print("--------Lowercase Evaluation--------")
-        print(f"Baseline F1: {self.baseline.evaluate(eval_dataset=self.lower_test)['eval_f1'] * 100:0.2f}")
-        print(f"DA F1: {self.da.evaluate(eval_dataset=self.lower_test)['eval_f1'] * 100:0.2f}")
-        print("--------Uppercase Evaluation--------")
-        print(f"Baseline F1: {self.baseline.evaluate(eval_dataset=self.upper_test)['eval_f1'] * 100:0.2f}")
-        print(f"DA F1: {self.da.evaluate(eval_dataset=self.upper_test)['eval_f1'] * 100:0.2f}")
+        print(f"Original Dataset F1: {self.model.evaluate(eval_dataset=self.conll['test'])['eval_f1'] * 100:0.2f}")
+        print(f"Lowercase Dataset F1: {self.model.evaluate(eval_dataset=self.lower_test)['eval_f1'] * 100:0.2f}")
+        print(f"Uppercase F1: {self.model.evaluate(eval_dataset=self.upper_test)['eval_f1'] * 100:0.2f}")
 
-    def compute_objective(self, metrics: dict[str, float]) -> float:
+    @staticmethod
+    def compute_objective(metrics: dict[str, float]) -> float:
         """
         The default objective to maximize/minimize when doing an hyperparameter search. It is the evaluation loss if no
         metrics are provided to the :class:`~transformers.Trainer`, the sum of all metrics otherwise.
@@ -136,41 +138,73 @@ class NERModel:
         return f1 if len(metrics) == 0 else sum(metrics.values())
 
     def finetune(self):
-        self.baseline = Trainer(
-            model_init=self.model_init,
-            args=self.training_args,
-            train_dataset=self.conll["train"],
-            eval_dataset=self.conll["test"],
-            processing_class=self.tokenizer,
-            data_collator=self.data_collator,
-            compute_metrics=self.compute_metrics,
-        )
+        if self.da:
+            trainer = Trainer(
+                model_init=self.model_init,
+                args=self.training_args,
+                train_dataset=self.da_train_data,
+                eval_dataset=self.conll["validation"],
+                processing_class=self.tokenizer,
+                data_collator=self.data_collator,
+                compute_metrics=self.compute_metrics,
+            )
 
-        return self.baseline.hyperparameter_search(
-            direction='maximize',
-            compute_objective=self.compute_objective,
-            n_trials=10
-        )
+            return trainer.hyperparameter_search(
+                direction='maximize',
+                compute_objective=self.compute_objective,
+                n_trials=10
+            )
+        else:
+            trainer = Trainer(
+                model_init=self.model_init,
+                args=self.training_args,
+                train_dataset=self.conll["train"],
+                eval_dataset=self.conll["validation"],
+                processing_class=self.tokenizer,
+                data_collator=self.data_collator,
+                compute_metrics=self.compute_metrics,
+            )
+
+            return trainer.hyperparameter_search(
+                direction='maximize',
+                compute_objective=self.compute_objective,
+                n_trials=10
+            )
 
 if __name__ == '__main__':
     print(80 * '=')
-    print('BERT-base-cased')
+    print('BERT-base-cased Baseline')
     print(80 * '=')
-    bert_ner = NERModel('bert-base-cased')
-    bert_ner.tokenize_and_align_all_data()
-    # bert_ner.baseline_train()
-    # bert_ner.da_train()
-    # bert_ner.eval()
-    print(bert_ner.finetune())
+    baseline_bert_ner = NERModel('bert-base-cased', 8.89326919195178e-05,
+                                 64, 5, 40)
+    baseline_bert_ner.tokenize_and_align_all_data()
+    baseline_bert_ner.train()
+    baseline_bert_ner.eval()
 
-    # print(80 * '=')
-    # print('XLM-R Base')
-    # print(80 * '=')
-    # xlmr_ner = NERModel('xlm-roberta-base')
-    # xlmr_ner.tokenize_and_align_all_data()
-    # xlmr_ner.baseline_train()
-    # xlmr_ner.da_train()
-    # xlmr_ner.eval()
+    print(80 * '=')
+    print('BERT-base-cased Data Augmented')
+    print(80 * '=')
+    da_bert_ner = NERModel('bert-base-cased', da=True)
+    da_bert_ner.tokenize_and_align_all_data()
+    da_bert_ner.train()
+    da_bert_ner.eval()
+    # print(da_bert_ner.finetune())
+
+    print(80 * '=')
+    print('XLM-R Baseline')
+    print(80 * '=')
+    baseline_xlmr_ner = NERModel('xlm-roberta-base')
+    baseline_xlmr_ner.tokenize_and_align_all_data()
+    baseline_xlmr_ner.train()
+    baseline_xlmr_ner.eval()
+
+    print(80 * '=')
+    print('XLM-R Data Augmented')
+    print(80 * '=')
+    da_xlmr_ner = NERModel('xlm-roberta-base', da=True)
+    da_xlmr_ner.tokenize_and_align_all_data()
+    da_xlmr_ner.train()
+    da_xlmr_ner.eval()
 
     print('Done!')
 
